546676efbfcfd7ee95068c8ed3a719b5b72aa31a
arif3179 <rahmawan1@gmail.com>
2019-12-07 15:48:37 +0700

    update mencari modus

--- mencari-Modus.js
+++ mencari-Modus.js
@@ -1,38 +1,32 @@
 function cariModus(arr) {
-    var modusArray = []
-    for(i = 0; i < arr.length; i++){
-      var unique = true
-      for(j = 0; j < modusArray.length;j++){
-        if(arr[i] === modusArray[j][0]){
-          modusArray[j][1]++ 
-          unique = false
-        }
+  var output = []
+  for(i=0;i<arr.length;i++){
+    var angkaBaru = true;
+    for(j=0;j<output.length;j++){
+      if( arr[i] == output[j][0]){
+        output[j][1]++
+        angkaBaru = false
       }
-      if(unique){
-        modusArray.push([arr[i], 1])
-      }
+    } 
+    if(angkaBaru){
+      output.push([arr[i],1])
     }
-    if(modusArray.length <= 1){
-      return -1
-    }
-    var countModus = 0
-    var biggestModus = ''
-    var notModusNumber = 1
-    for(i = 0; i < modusArray.length;i++){
-      if(countModus === 1){
-        notModusNumber++
-      }
-      if(modusArray[i][1] > countModus){
-        countModus = modusArray[i][1]
-        biggestModus = modusArray[i][0]
-      }
-    }
-    return notModusNumber === modusArray.length ? -1 : biggestModus
+  } if(output.length == arr.length || output.length == 1){
+    return -1
   }
+  var pembanding = 0;
+  var hasilIndex = 0;
+  for(k=0;k<output.length;k++){
+    if(output[k][1] > pembanding){
+      pembanding = output[k][1]
+      hasilIndex = output[k][0]
+     } 
+   }
+ return hasilIndex
+}
   // TEST CASES
-  console.log(cariModus([10, 4,4,4,4, 5, 2, 4])); // 4
-   //console.log(cariModus([5, 10, 10, 6, 5])); // 5
-   //console.log(cariModus([10, 3, 1, 2, 5])); // -1
-   //console.log(cariModus([1, 2, 3, 3, 4, 5])); // 3
-   //console.log(cariModus([7, 7, 7, 7, 7])); // -1
-   //console.log(cariModus([1,2,3,4,6,7,8,5,4,3,2,9,2,3,4,5]))
+console.log(cariModus([10, 4, 5, 2, 4])); // 4
+console.log(cariModus([5, 10, 10, 6, 5])); // 5
+console.log(cariModus([10, 3, 1, 2, 5])); // -1
+console.log(cariModus([1, 2, 3, 3, 4, 5])); // 3
+console.log(cariModus([7, 7, 7, 7, 7])); // -1
